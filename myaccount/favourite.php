d7037d3a3f0805a71b390177a13f39441e1934e4
ADC4869 <thanhdattran123789@gmail.com>
2024-11-08 17:41:18 +0700

    DAT

--- myaccount/favourite.php
+++ myaccount/favourite.php
@@ -18,13 +18,14 @@
     <link rel="stylesheet" href="../bootstrap/dist/js/bootstrap.bundle.min.js">
     <!-- Css -->
     <link rel="stylesheet" href="./css/favourite.css">
+    <link rel="stylesheet" href="../css/global.css">
 </head>
 <body>
     <header>
         <div class="container">
             <!-- Header -->
             <div class="header">
-                <i class="fas fa-arrow-left"></i> Yêu thích
+                <i class="fas fa-arrow-left" onclick="goBack()"></i> Yêu thích
             </div>
 
             <!-- Cards Section -->
@@ -41,5 +42,47 @@
             </div>
         </div>
     </header>
+
+    <main>
+        <div class="container my-4">
+            <h5 class="fw-bold">Lựa chọn đặt món hàng đầu</h5>
+            <div class="scroll-container">
+
+                <div class="food-card position-relative">
+                    <img src="../img/bun.jpg" alt="Bún Bò Gốc Huế">
+                    <span class="discount-tag">Giảm 50%</span>
+                    <div class="food-content">
+                        <p class="food-title">Bún Bò Gốc Huế OMI</p>
+                        <p class="food-details">1.6 km <span class="text-warning"><i class="fas fa-star"></i> 4.7</span>
+                        </p>
+                        <p class="text-success">Lên đến 50%</p>
+                    </div>
+                </div>
+
+                <div class="food-card position-relative">
+                    <img src="../img/bun1.webp" alt="Bún Măng Vịt">
+                    <span class="discount-tag">Giảm 20%</span>
+                    <div class="food-content">
+                        <p class="food-title">BÚN MĂNG VỊT - BÚN CHẢ GIÒ ĐÌ LOAN</p>
+                        <p class="food-details">2.5 km <span class="text-warning"><i class="fas fa-star"></i> 4.2</span>
+                        </p>
+                        <p class="text-success">Giảm 20%</p>
+                    </div>
+                </div>
+
+                <div class="food-card position-relative">
+                    <img src="../img/com.webp" alt="Cơm Tấm Sườn">
+                    <span class="discount-tag">Giảm 10%</span>
+                    <div class="food-content">
+                        <p class="food-title">Cơm Tấm Sườn Bì Chả</p>
+                        <p class="food-details">5.0 km <span class="text-warning"><i class="fas fa-star"></i> 4.5</span>
+                        </p>
+                        <p class="text-success">Lên đến 10%</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </body>
+<script src="../js/back.js"></script>
 </html>
